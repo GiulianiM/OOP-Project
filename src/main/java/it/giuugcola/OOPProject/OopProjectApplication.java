c0756cea64ee0df9cf8a0b4035a6dd226a3e1bce
GiulianiM <s1098185@studenti.univpm.it>
2021-12-02 19:37:37 +0100

    Initial commit

---
+++ src/main/java/it/giuugcola/OOPProject/OopProjectApplication.java
@@ -0,0 +1,13 @@
+package it.giuugcola.OOPProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/giuugcola/OOPProject/OopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.giuugcola.OOPProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
